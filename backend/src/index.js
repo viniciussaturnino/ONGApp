c79bdf9601eb8d2dcbd49ec6fbd73a7e0bc84216
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-03-26 02:28:27 -0300

    Finished frontend project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+const app = express();
 
 
-app.listen(3333)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
