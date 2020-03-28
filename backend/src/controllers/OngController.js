268533ffd6ba207abc88ac72a3b164d871d82729
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-03-27 22:11:03 -0300

    Added delete method for eventual tests in backend and added description in more details on app mobile

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -23,5 +23,21 @@ module.exports = {
         })
     
         return response.json({ id });
+    },
+
+    async delete(request, response){
+        const { id } = request.params;
+
+        const ong = await connection('ongs')
+        .where('id', id)
+        .first();
+
+        if(ong.id !== id){
+            return response.status(401).json({ error: 'Operation not permitted.' });
+        }
+
+        await connection('ongs').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };
