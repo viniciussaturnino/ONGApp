83c32ff843efff90285b943faa4b8d8ac3cc2676
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-03-29 16:36:19 -0300

    added unit and integration tests

--- backend/src/database/migrations/20200324162545_create_ongs.js
+++ backend/src/database/migrations/20200324162545_create_ongs.js
@@ -11,5 +11,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dopTable('ongs');
+  return knex.schema.dropTable('ongs');
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,17 +16,31 @@ routes.post('/ongs', celebrate({
     [Segments.BODY] : Joi.object().keys({
         name: Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(11),
+        whatsapp: Joi.string().required().min(12).max(13),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
 }), OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+    [Segments.HEADERS] : Joi.object({
+        authorization : Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
 
 routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY] : Joi.object().keys({
+        page : Joi.number(),
+    })
+}), IncidentController.index);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS] : Joi.object().keys({
+        id : Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 
 module.exports = routes;
